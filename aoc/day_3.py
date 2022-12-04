9c9966ad30a0d6d3d2a8e62d57ba0bfd94c4d820
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-12-04 06:32:05 -0700

    Day 3 impl

---
+++ aoc/day_3.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from typing import Any
+
+# from aoc.utils import unimplemented
+from aoc.utils import read_input_data
+
+GroupT = tuple[str, str ,str]
+GroupsT = list[tuple[GroupT, GroupT]]
+
+
+def read_data() -> Any:
+    day = __file__.split(".")[0][-1]
+    path = Path(f"data/day_{day}.txt")
+    data = read_input_data(path).strip().split("\n")
+    return data
+
+
+def split_line(line: str) -> tuple[str, str]:
+    middle = len(line) // 2
+    return (line[:middle], line[middle:])
+
+
+def get_value(char: str) -> int:
+    return (
+        ord(char) - 38
+        if char.isupper()
+        else ord(char) - 96
+    )
+
+
+# @unimplemented
+def question_1() -> Any:
+    data = read_data()
+    total = 0
+
+    for line in data:
+        left, right = split_line(line)
+        total += get_value(list(filter(lambda l: l in right, left))[0])
+
+    return total
+
+
+def group_elves(data: list[str]) -> list[list[str]]:
+    grouped: list[list[str]] = []
+    group: list[str] = []
+    idx = 0
+
+    for line in data:
+        if idx % 3 == 0 and idx != 0:
+            grouped.append(group)
+            group = []
+
+        group.append(line)
+        idx += 1
+
+    return grouped
+
+
+def group_by_x(x: int, groups: Any) -> Any:
+    return list(zip(*([iter(groups)] * x)))
+
+
+def split_group(group: tuple[str, str, str]) -> str:
+    return list(filter(lambda s: s in group[1] and s in group[2], group[0]))[0]
+
+
+def to_value(group: tuple[str, str, str]) -> int:
+    return get_value(split_group(group))
+
+
+# @unimplemented
+def question_2() -> Any:
+    data = read_data()
+    groups = group_by_x(3, data)
+    groups: GroupsT = group_by_x(2, groups)
+    return sum(map(lambda g: to_value(g[0]) + to_value(g[1]), groups))
